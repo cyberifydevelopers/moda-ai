3225a56917d232cafadb68b086465b1c150665db
zubairkofficial <thezubairkhan.developer@gmail.com>
2025-06-26 19:51:55 +0500

    basic setup + new routes

---
+++ app/routes/login.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/login";
+import { Login } from "~/auth/login";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function LoginPage() {
+  return <Login />;
+}
