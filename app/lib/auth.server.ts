2049863263aabb2aa419cb5c81f677af59bae574
zubairkofficial <thezubairkhan.developer@gmail.com>
2025-06-27 15:48:34 +0500

    add twilio code setup

--- app/lib/auth.server.ts
+++ app/lib/auth.server.ts
@@ -1,12 +1,24 @@
 import { betterAuth } from "better-auth"
 import { phoneNumber } from "better-auth/plugins"
+import twilio from "twilio"
  
 export const auth = betterAuth({
      plugins: [ 
         phoneNumber({  
             sendOTP: ({ phoneNumber, code }, request) => { 
                 // Implement sending OTP code via SMS
-                
+                const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_API_KEY_SECRET);
+                client.verify.v2.services(
+                    "VAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                ).verifications.create({
+                    to: phoneNumber,
+                    channel: "sms",
+                    customCode: code
+                }).then(verification => {
+                    console.log(`OTP sent to ${phoneNumber}: ${verification.status}`);
+                }).catch(error => {
+                    console.error(`Failed to send OTP to ${phoneNumber}:`, error);
+                })
             } 
         }) 
     ],
